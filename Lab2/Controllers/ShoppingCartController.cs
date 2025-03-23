b9d48b31c188ed6302df83c31ba99684b5c0596d
alexandralesan1 <163538340+alexandralesan1@users.noreply.github.com>
2025-03-23 20:26:36 +0200

    Add files via upload

---
+++ Lab2/Controllers/ShoppingCartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: Promotion
+        public ActionResult ShoppingCart()
+        {
+            return View();
+        }
+    }
+}
